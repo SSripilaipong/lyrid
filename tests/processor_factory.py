066778786517c177b28b5212c6406c2cb709e656
Santhapon Sripilaipong <santhapon@senestia.com>
2022-10-13 16:35:35 +0700

    feat(processor): test_should_pass_non_processor_command_to_handle

--- tests/processor_factory.py
+++ tests/processor_factory.py
@@ -1,12 +1,14 @@
 import multiprocessing as mp
+from typing import Callable
 
 from lyrid.base import ProcessorBase
 from lyrid.core.processor import Command
 
 
-def create_processor(*, command_queue: mp.Queue = None) -> ProcessorBase:
+def create_processor(*, command_queue: mp.Queue = None, handle: Callable[[Command], None] = None) -> ProcessorBase:
     command_queue = command_queue or mp.Queue()
-    return ProcessorBase(handle=handle_mock, command_queue=command_queue)
+    handle = handle or handle_mock
+    return ProcessorBase(handle=handle, command_queue=command_queue)
 
 
 def handle_mock(_: Command):
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
+from dataclasses import dataclass
 
-from lyrid.core.processor import ProcessorStopCommand
+from lyrid.core.processor import ProcessorStopCommand, Command
 from tests.processor_factory import create_processor
 
 
@@ -15,3 +16,24 @@ def test_should_stop_processor_loop_by_processor_stop_command():
     command_queue.put(ProcessorStopCommand())
     processor = create_processor(command_queue=command_queue)
     processor.processor_loop()
+
+
+def test_should_pass_non_processor_command_to_handle():
+    def handle(command: Command):
+        handle.command = command
+
+    handle.command = None
+
+    command_queue = mp.Queue()
+    command_queue.put(MyCommand("Hello"))
+    command_queue.put(ProcessorStopCommand())
+
+    processor = create_processor(command_queue=command_queue, handle=handle)
+    processor.processor_loop()
+
+    assert handle.command == MyCommand("Hello")
+
+
+@dataclass
+class MyCommand:
+    value: str
