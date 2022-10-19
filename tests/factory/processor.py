0673610b959145e2aeda0b74e63958fff54d8ce1
Santhapon Sripilaipong <santhapon@senestia.com>
2022-10-19 23:00:48 +0700

    refactor(tests.processor): replace type annotation mp.Queue with queue.Queue

--- tests/factory/processor.py
+++ tests/factory/processor.py
@@ -1,11 +1,12 @@
 import multiprocessing as mp
+import queue
 from typing import Callable
 
 from lyrid.base import ProcessorBase
 from lyrid.core.processor import Command
 
 
-def create_processor(*, command_queue: mp.Queue = None, handle: Callable[[Command], None] = None) -> ProcessorBase:
+def create_processor(*, command_queue: queue.Queue = None, handle: Callable[[Command], None] = None) -> ProcessorBase:
     command_queue = command_queue or mp.Queue()
     handle = handle or handle_mock
     return ProcessorBase(handle=handle, command_queue=command_queue)
