e35f518e096c21382123a092582e724a721e0d2e
SSripilaipong <SHSnail@mail.com>
2022-12-04 17:25:30 +0700

    refactor(lyrid): add Address to lyrid package
    Refs: #9

--- lyrid/__init__.py
+++ lyrid/__init__.py
@@ -1,4 +1,4 @@
 from lyrid._actor_system import ActorSystem
 from lyrid.api.actor import StatefulActor, field, Switch
 from lyrid.base import Actor as VanillaActor
-from lyrid.core.messaging import Message
+from lyrid.core.messaging import Message, Address
