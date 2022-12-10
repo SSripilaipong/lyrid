29fe0ec7b7ee7dfd7116570e11cb562138e411eb
SSripilaipong <SHSnail@mail.com>
2022-12-10 19:31:25 +0700

    feat(system.actor_placement.policy.round_robin): test_should_repeat_the_same_order_again_when_getting_more_than_n_times
    Refs: #15

--- lyrid/base/_system/_placement/_policy.py
+++ lyrid/base/_system/_placement/_policy.py
@@ -9,13 +9,18 @@ class RoundRobin(PlacementPolicy):
     def __init__(self):
         self._addresses = []
         self._index = 0
+        self._n = 0
 
     def set_node_addresses(self, addresses: List[Address]):
         self._addresses = addresses.copy()
         self._index = 0
+        self._n = len(self._addresses)
+
         random.shuffle(self._addresses)
 
     def get_placement_node(self) -> Address:
         node = self._addresses[self._index]
         self._index += 1
+        if self._index >= self._n:
+            self._index = 0
         return node
--- tests/system/actor_placement/policy/test_round_robin.py
+++ tests/system/actor_placement/policy/test_round_robin.py
@@ -2,9 +2,17 @@ from lyrid import Address
 from lyrid.system import RoundRobin
 
 
-def test_should_return_all_node_addresses_without_repeating_in_any_order():
+def test_should_return_all_node_addresses_without_repeating_in_any_order_when_getting_node_n_times():
     policy = RoundRobin()
     policy.set_node_addresses([Address("#node0"), Address("#node1"), Address("#node2")])
 
     assert {policy.get_placement_node(), policy.get_placement_node(), policy.get_placement_node()} == \
            {Address("#node0"), Address("#node1"), Address("#node2")}
+
+
+def test_should_repeat_the_same_order_again_when_getting_more_than_n_times():
+    policy = RoundRobin()
+    policy.set_node_addresses([Address("#node0"), Address("#node1"), Address("#node2")])
+
+    assert [policy.get_placement_node(), policy.get_placement_node(), policy.get_placement_node()] == \
+           [policy.get_placement_node(), policy.get_placement_node(), policy.get_placement_node()]
