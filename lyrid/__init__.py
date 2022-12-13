651eccf7753e0696cfd9b72c273da5c06674deaf
SSripilaipong <SHSnail@mail.com>
2022-12-13 22:05:19 +0700

    refactor(lyrid): remove Reply from top-level package since replaced by actor.Reply
    Refs: #14

--- lyrid/__init__.py
+++ lyrid/__init__.py
@@ -3,7 +3,7 @@ from lyrid.api.actor import StatefulActor, field, Switch
 from lyrid.base import Actor as VanillaActor
 from lyrid.base.system.placement import MatchAll, MatchType, RoundRobin
 from lyrid.core.background_task import BackgroundTaskExited
-from lyrid.core.messaging import Message, Address, Ask, Reply
+from lyrid.core.messaging import Message, Address, Ask
 from lyrid.core.process import ChildStopped
 from lyrid.core.process import ProcessContext
 from lyrid.core.system import PlacementPolicy, PlacementPolicyMatcher, Placement, SpawnChildCompleted
