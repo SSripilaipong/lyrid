fb731de8a15e44c33ce90ad1900123bdedb3d4e7
SSripilaipong <SHSnail@mail.com>
2022-12-25 19:33:11 +0700

    release: 0.0.5
    Refs: #14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 NAME = 'lyrid'
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 URL = 'https://github.com/SSripilaipong/lyrid'
 LICENSE = 'MIT'
 AUTHOR = 'SSripilaipong'
