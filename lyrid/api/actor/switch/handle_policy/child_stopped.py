5d8c987be4b9f8e26f062e30af51b250d50a9f52
SSripilaipong <SHSnail@mail.com>
2022-12-31 17:27:09 +0700

    feat(api.actor.switch.child_stopped.validation): test_should_raise_type_error_when_address_argument_is_specified_with_wrong_type_annotation
    Refs: #9

--- lyrid/api/actor/switch/handle_policy/child_stopped.py
+++ lyrid/api/actor/switch/handle_policy/child_stopped.py
@@ -2,7 +2,8 @@ import inspect
 from dataclasses import dataclass
 from typing import Callable, Optional, Type, Dict, Any
 
-from lyrid.api.actor.switch.handle_policy.error_message import invalid_argument_for_method_error
+from lyrid.api.actor.switch.handle_policy.error_message import invalid_argument_for_method_error, \
+    argument_in_method_must_be_annotated_as_type_error
 from lyrid.api.actor.switch.handle_rule import HandlePolicy, HandleRule
 from lyrid.base.actor import Actor
 from lyrid.core.messaging import Address, Message
@@ -28,6 +29,8 @@ class ChildStoppedHandlePolicy(HandlePolicy):
             if name == "self":
                 continue
             elif name == "address":
+                if param.annotation is not Address:
+                    raise TypeError(argument_in_method_must_be_annotated_as_type_error(name, function_name, "Address"))
                 required_params.address = True
             elif name == "exception":
                 required_params.exception = True
--- tests/api/actor/switch/child_stopped/test_validation.py
+++ tests/api/actor/switch/child_stopped/test_validation.py
@@ -1,7 +1,8 @@
 import pytest
 
 from lyrid import use_switch, Actor, switch, Address
-from lyrid.api.actor.switch.handle_policy.error_message import invalid_argument_for_method_error
+from lyrid.api.actor.switch.handle_policy.error_message import invalid_argument_for_method_error, \
+    argument_in_method_must_be_annotated_as_type_error
 
 
 def test_should_raise_type_error_when_invalid_argument_name_is_specified():
@@ -13,3 +14,14 @@ def test_should_raise_type_error_when_invalid_argument_name_is_specified():
                 pass
 
     assert str(e.value) == str(invalid_argument_for_method_error('xxx', 'my_handler'))
+
+
+def test_should_raise_type_error_when_address_argument_is_specified_with_wrong_type_annotation():
+    with pytest.raises(TypeError) as e:
+        @use_switch
+        class A(Actor):
+            @switch.child_stopped(exception=None)
+            def handle(self, address: int):
+                pass
+
+    assert str(e.value) == str(argument_in_method_must_be_annotated_as_type_error("address", "handle", "Address"))
