d7048083ffe0d957fde4fc93ef807f2553b1d87b
SSripilaipong <SHSnail@mail.com>
2023-01-11 23:18:08 +0700

    release: 0.0.9 with README and LICENSE
    Refs: #18

---
+++ README.md
@@ -0,0 +1,30 @@
+Lyrid
+==============
+
+[![PyPi Version](https://img.shields.io/pypi/v/lyrid)](https://pypi.org/project/lyrid/)
+[![Supported Python Versions](https://img.shields.io/pypi/pyversions/lyrid.svg)](https://pypi.org/project/lyrid/)
+[![License](https://img.shields.io/github/license/ssripilaipong/lyrid)](https://github.com/SSripilaipong/lyrid/blob/master/LICENSE.md)
+
+# Overview
+
+An actor model framework that simplifies concurrent system while support real parallelism.
+<br>
+
+No thread/process/async/await, just actor. Implemented in pure Python.'
+
+# Requirements
+
+- Python 3.8+
+- Works on Linux, Windows, macOS, BSD
+
+# Installation
+
+using pip:
+
+```
+pip install lyrid
+```
+
+# Documentation
+
+[Github Wiki](https://github.com/SSripilaipong/lyrid/wiki)
--- setup.py
+++ setup.py
@@ -1,15 +1,40 @@
+import pypandoc
 import setuptools
 from setuptools import setup
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+LONG_DESCRIPTION = pypandoc.convert_file('README.md', 'rst')
+
 NAME = 'lyrid'
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 URL = 'https://github.com/SSripilaipong/lyrid'
 LICENSE = 'MIT'
 AUTHOR = 'SSripilaipong'
 EMAIL = 'SHSnail@mail.com'
+
+DESCRIPTION = ('An actor model framework that simplifies concurrent system while support real parallelism. '
+               'No thread/process/async/await, just actor. Implemented in pure Python.')
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: System :: Distributed Computing',
+]
 
 setup(
     name=NAME,
@@ -21,10 +46,10 @@ setup(
     license=LICENSE,
     author=AUTHOR,
     author_email=EMAIL,
-    description=None,
-    long_description=None,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     python_requires='>=3.8',
     install_requires=requirements,
-    classifiers=[],
+    classifiers=CLASSIFIERS,
     include_package_data=True,
 )
