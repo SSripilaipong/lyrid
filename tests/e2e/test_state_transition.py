734331dae13819f53981361f497ebd8819fd917f
SSripilaipong <SHSnail@mail.com>
2023-01-15 13:40:54 +0700

    refactor(tests.e2e.state_transition): refactor minor design for readability to match with example in document
    Refs: #18

--- tests/e2e/test_state_transition.py
+++ tests/e2e/test_state_transition.py
@@ -10,7 +10,7 @@ class WhoAreYou(Message):
 
 @dataclass
 class IAm(Message):
-    type_: str
+    name: str
 
 
 class MakeHimAngry(Message):
@@ -21,34 +21,38 @@ class CalmDown(Message):
     pass
 
 
+@dataclass
 class Base(Actor):
+    name: str
 
     @switch.ask(type=WhoAreYou)
     def who_are_you(self, sender: Address, ref_id: str):
-        self.reply(sender, IAm(self.__class__.__name__), ref_id=ref_id)
+        self.reply(sender, IAm(name=self.name), ref_id=ref_id)
 
 
 @use_switch
+@dataclass
 class Banner(Base):
 
     @switch.message(type=MakeHimAngry)
     def make_him_angry(self):
-        self.become(Hulk())
+        self.become(Hulk(name="Hulk"))
 
 
 @use_switch
+@dataclass
 class Hulk(Base):
 
     @switch.message(type=CalmDown)
     def calm_down(self):
-        self.become(Banner())
+        self.become(Banner(name="Banner"))
 
 
 def test_should_transit_state_back_and_forth():
     system = ActorSystem(n_nodes=1)
 
     try:
-        banner = system.spawn(Banner(), key="banner")
+        banner = system.spawn(Banner(name="Banner"), key="banner")
         time.sleep(0.03)
 
         assert system.ask(banner, WhoAreYou()) == IAm('Banner')
